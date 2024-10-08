a76b3c29a8a16724f8dbaff061bd656badd13870
Hasan1375-tech <karami.samdad@gmail.com>
2024-10-08 11:05:32 +0330

    add [CategoryId] to [Ticket] entity and add migration for that

---
+++ src/Infrastructure/Domic.Persistence/Migrations/C/20241008073451_AddCategoryIdToTicketEntity.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Domic.Persistence.Migrations.C
+{
+    public partial class AddCategoryIdToTicketEntity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CategoryId",
+                table: "Tickets",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CategoryId",
+                table: "Tickets");
+        }
+    }
+}
