57177591390b90122b31bc5de8097d935ad6bd97
Hasan1375-tech <karami.samdad@gmail.com>
2025-08-21 12:38:08 +0330

    update

--- src/Core/Domic.UseCase/TicketUseCase/DTOs/TicketDto.cs
+++ src/Core/Domic.UseCase/TicketUseCase/DTOs/TicketDto.cs
@@ -4,8 +4,11 @@ namespace Domic.UseCase.TicketUseCase.DTOs;
 
 public class TicketDto
 {
+    public string Id { get; set; }
     public string Title { get; set; }
     public string Description { get; set; }
     public Status Status { get; set; }
+    public string StatusTitle { get; set; }
     public Priority Priority { get; set; }
+    public string PriorityTitle { get; set; }
 }
--- src/Core/Domic.UseCase/TicketUseCase/Queries/ReadAllPaginate/ReadAllPaginatedQueryHandler.cs
+++ src/Core/Domic.UseCase/TicketUseCase/Queries/ReadAllPaginate/ReadAllPaginatedQueryHandler.cs
@@ -1,5 +1,6 @@
 ﻿using Domic.Core.UseCase.Contracts.Interfaces;
 using Domic.Domain.Ticket.Contracts.Interfaces;
+using Domic.Domain.Ticket.Enumerations;
 using Domic.UseCase.TicketUseCase.DTOs;
 
 namespace Domic.UseCase.TicketUseCase.Queries.ReadAllPaginate;
@@ -12,10 +13,19 @@ public class ReadAllPaginatedQueryHandler(ITicketCommandRepository ticketCommand
         var result =
             await ticketCommandRepository.FindAllWithPaginateByProjectionAsync(ticket => 
                 new TicketDto {
+                    Id = ticket.Id,
                     Title = ticket.Title.Value,
                     Description = ticket.Description.Value,
                     Status = ticket.Status,
-                    Priority = ticket.Priority
+                    StatusTitle = ticket.Status == Status.Close ? "بسته شده" : (
+                        ticket.Status == Status.Waiting ? "در انتظار پاسخ" : "بسته شده"
+                    ),
+                    Priority = ticket.Priority,
+                    PriorityTitle = ticket.Priority == Priority.Critical ? "بحرانی" : (
+                        ticket.Priority == Priority.High ? "اولویت بالا" : (
+                            ticket.Priority == Priority.Mid ? "اولویت متوسط" : "اولویت پایین"
+                        )
+                    )
                 }, 
                 query.CountPerPage.Value, query.PageNumber.Value, cancellationToken
             );
